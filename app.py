838f39c00cecc9c12b21f362e711088ae42b0db0
MPrav143 <praveen17082005@gmail.com>
2025-08-27 09:35:06 +0530

    Update app.py with dashboard UI and fix requirements

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 
 # Set page config
 st.set_page_config(page_title="Adidas Dashboard", layout="wide")
-st.markdown('<style>div.block-container{padding-top:1rem}</style>', unsafe_allow_html=True)
+st.markdown('<style>div.block-container{padding-top:10 rem}</style>', unsafe_allow_html=True)
 
 # Load Excel file
 try:
